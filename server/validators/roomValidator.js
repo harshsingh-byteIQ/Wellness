9284758f2118f937d42f3f475212890115442acd
harshsingh-byteIQ <163847999+harshsingh-byteIQ@users.noreply.github.com>
2025-02-20 20:21:30 +0530

    Delete server directory

--- server/validators/roomValidator.js
+++
@@ -1,7 +0,0 @@
-const { body } = require("express-validator");
-
-const roomValidationRules = [body("roomCode").isString().withMessage("Password is required").trim().notEmpty().withMessage("Room code is required"), body("password").isString().withMessage("Password is required").trim().notEmpty().withMessage("Password is required")];
-
-module.exports = {
-    roomValidationRules,
-};
