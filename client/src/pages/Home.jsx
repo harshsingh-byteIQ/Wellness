b41c3fe5c0d45f1d9e9b7b49b0e2222e41dd26f9
harshsingh-byteIQ <harshsingh.byteiq@gmail.com>
2025-04-06 09:37:30 +0530

    new url for meeting 0n 6/4/25

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             setPasswordError("");
             setCreateRoomLoading(true);
 
-            const response = await fetch(`https://a40e-2405-201-a007-73-e860-f705-b5a3-3ce.ngrok-free.app/room/create`, {
+            const response = await fetch(`https://8b09-2405-201-a007-73-cd45-51e-c8b2-e26c.ngrok-free.app /room/create`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -62,7 +62,7 @@ const Home = () => {
             setPasswordError("");
             setJoinRoomLoading(true);
 
-            const response = await fetch(`https://a40e-2405-201-a007-73-e860-f705-b5a3-3ce.ngrok-free.app/room/verify`, {
+            const response = await fetch(`https://8b09-2405-201-a007-73-cd45-51e-c8b2-e26c.ngrok-free.app /room/verify`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
